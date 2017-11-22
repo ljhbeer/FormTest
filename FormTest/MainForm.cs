5cdfb7e3d895cf2a1c5b6cdf86c4dc0dd484da6c
ljh_beer <ljh_beer@163.com>
2017-11-22 13:05:12 +0800

    v1.3 ChangJpgTo 2value.Jpg

--- FormTest/MainForm.cs
+++ FormTest/MainForm.cs
@@ -19,19 +19,42 @@ namespace FormTest
 		public MainForm()
 		{
 			InitializeComponent();
-			textBoxWorkPath.Text = @"E:\Project\SWAR\back\C_IMAGES\200001_00_1_p1.TIF";
+            textBoxWorkPath.Text = @"E:\Temp\JPG\694.jpg"; //468.jpg  697.jpg
 		}		
 		void ButtonTestClick(object sender, EventArgs e)
 		{
-			string imgfilename = textBoxWorkPath.Text;
             System.DateTime dt1 = System.DateTime.Now;
-          
 
+            FormatJpgToTif();
 
             System.DateTime dt2 = System.DateTime.Now;
             System.TimeSpan ts = dt2 - dt1;
             MessageBox.Show("耗时"+ts.Minutes*60+ts.Seconds+"."+ts.Milliseconds*1.0/1000.0+"秒");
 		}
+
+        private void FormatJpgToTif()
+        {
+            string imgfilename = textBoxWorkPath.Text;
+            Bitmap bmp =(Bitmap) Bitmap.FromFile(imgfilename);
+
+            //Tools.BitmapTools.Gray(bmp);
+            Rectangle r = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            Rectangle r1 = new Rectangle(0, 0, bmp.Width / 2, bmp.Height);
+            int gramm = Tools.BitmapTools.GetstatisticGamma(bmp, 5, r1);
+
+            Bitmap bmpdst = new Bitmap(r.Width, r.Height);
+            Tools.BitmapTools.GammaImg(bmpdst, bmp, gramm, r);
+
+
+
+
+
+
+
+            bmpdst.Save( imgfilename.Replace(".jpg","_gramm_1.jpg"));
+
+            MessageBox.Show("OK");
+        }
 
 	}
 	
