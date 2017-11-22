fdbe7efc13beebdaaad01fa27ad0098061dcdb6c
ljh_beer <ljh_beer@163.com>
2017-11-22 12:12:05 +0800

    formTest1.0

---
+++ FormTest/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2017-10-20
+ * 时间: 11:48
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+namespace FormTest
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxWorkPath = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.buttonTest = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBoxWorkPath
+			// 
+			this.textBoxWorkPath.Location = new System.Drawing.Point(95, 6);
+			this.textBoxWorkPath.Name = "textBoxWorkPath";
+			this.textBoxWorkPath.Size = new System.Drawing.Size(311, 21);
+			this.textBoxWorkPath.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(14, 6);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 20);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "工作目录";
+			// 
+			// buttonTest
+			// 
+			this.buttonTest.Location = new System.Drawing.Point(136, 235);
+			this.buttonTest.Name = "buttonTest";
+			this.buttonTest.Size = new System.Drawing.Size(162, 37);
+			this.buttonTest.TabIndex = 2;
+			this.buttonTest.Text = "测试";
+			this.buttonTest.UseVisualStyleBackColor = true;
+			this.buttonTest.Click += new System.EventHandler(this.ButtonTestClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(503, 296);
+			this.Controls.Add(this.buttonTest);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxWorkPath);
+			this.Name = "MainForm";
+			this.Text = "FormTest";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button buttonTest;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBoxWorkPath;
+	}
+}
---
+++ FormTest/MainForm.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace FormTest
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			textBoxWorkPath.Text = @"E:\Project\SWAR\back\C_IMAGES\200001_00_1_p1.TIF";
+		}
+		
+		void ButtonTestClick(object sender, EventArgs e)
+		{
+			string imgfilename = textBoxWorkPath.Text;
+            List<string> namelist = NameList( imgfilename );
+            namelist = namelist.Take(1).ToList();
+
+            Rectangle imgrect = new Rectangle(1904,634,392,143);            
+            if (File.Exists("img.data"))
+                File.Delete("img.data");
+            System.DateTime dt1 = System.DateTime.Now;
+            List<datainfo> di = ConstructImgData1(namelist, ref imgrect);
+            System.DateTime dt2 = System.DateTime.Now;
+            System.TimeSpan ts = dt2 - dt1;
+            MessageBox.Show("耗时"+ts.Minutes*60+ts.Seconds+"."+ts.Milliseconds*1.0/1000.0+"秒");
+            ReadBitmap2(di);
+		}
+
+        private static List<datainfo> ConstructImgData1(List<string> namelist, ref Rectangle imgrect)
+        {
+            FileStream fs = new FileStream("img.data", FileMode.Append, FileAccess.Write);  
+            BufferedStream bs = new BufferedStream(fs, 40960);
+
+            Rectangle bigrect = new Rectangle( 1800, 600,  2712,  1629);
+            int startpos = 0;
+            List<datainfo> di = new List<datainfo>();
+            for (int cnt = 0; cnt < namelist.Count; cnt++)
+            {
+                Bitmap bmp = (Bitmap)Bitmap.FromFile(namelist[cnt]);
+                Bitmap bmpc = bmp.Clone(imgrect, bmp.PixelFormat);
+                bmpc.Save("bmpc.tif");
+                BitmapData bmpdata = bmp.LockBits(imgrect, ImageLockMode.ReadOnly, bmp.PixelFormat);
+                BitmapData bmpcdata = bmpc.LockBits(new Rectangle(0, 0, imgrect.Width, imgrect.Height), ImageLockMode.ReadOnly, bmpc.PixelFormat);
+
+                unsafe
+                {
+                    byte* bmpPtr = (byte*)bmpdata.Scan0.ToPointer();
+                    byte* bmpcPtr = (byte*)bmpcdata.Scan0.ToPointer();
+
+                    byte[] buff = new byte[bmpcdata.Stride * bmpcdata.Height];
+                   
+                    int cols = bmpdata.Width / 8;
+                    for (int y = 0; y < bmpdata.Height; y++)
+                    {
+                        for (int i = 0; i < cols; i++)
+                        {
+                            if (bmpPtr[i] != bmpcPtr[i])
+                            {
+                                MessageBox.Show("数据不一致");
+                            }
+                            buff[y * bmpcdata.Stride + i] = bmpcPtr[i];
+                        }
+                        bmpPtr += bmpdata.Stride;
+                        bmpcPtr += bmpcdata.Stride;
+                    }
+
+                    bs.Write(buff, 0, buff.Length);
+                }
+                MessageBox.Show("bmptr 与 bmpcPtr一致");
+
+
+                bmp.UnlockBits(bmpdata);
+                bmpc.UnlockBits(bmpcdata);
+                
+                di.Add(new datainfo(namelist[cnt], startpos, bs.Length));
+                startpos += (int)bs.Length;
+                //if (buff.Length != bs.Length)
+                //{
+                //    MessageBox.Show("Error: buff.length=" + buff.Length + ",bs.length=" + bs.Length);
+                //}
+            }
+            bs.Flush();
+            fs.Flush();
+            bs.Close();
+            fs.Close();
+            return di;
+        }
+        private static List<datainfo> ConstructImgData2(List<string> namelist, ref Rectangle imgrect) //Test bitmapdata.Lockbit
+        {
+            FileStream fs = new FileStream("img.data", FileMode.Append, FileAccess.Write);
+            BufferedStream bs = new BufferedStream(fs, 40960);
+
+            Rectangle bigrect = new Rectangle(1800, 600, 2712, 1629);
+            int startpos = 0;
+            List<datainfo> di = new List<datainfo>();
+            for (int cnt = 0; cnt < namelist.Count; cnt++)
+            {
+                Bitmap bmp = (Bitmap)Bitmap.FromFile(namelist[cnt]);
+                Bitmap bmpc = bmp.Clone(imgrect, bmp.PixelFormat);
+
+                BitmapData bmpalldata = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
+                BitmapData bmpcdata = bmpc.LockBits(new Rectangle(0, 0, imgrect.Width, imgrect.Height), ImageLockMode.ReadOnly, bmpc.PixelFormat);
+
+                unsafe  // Format1bppIndexed
+                {
+                    byte* bmpallPtr = (byte*)bmpalldata.Scan0.ToPointer();
+                    byte* bmpcPtr = (byte*)bmpcdata.Scan0.ToPointer();
+                   
+                    bmpallPtr += imgrect.Y * bmpalldata.Stride + imgrect.X/8;
+                    for (int y = 0; y < bmpcdata.Height; y++)
+                    {
+                        for (int i = 0; i < bmpcdata.Width / 8; i++)
+                        {
+                            if (bmpallPtr[i] != bmpcPtr[i])
+                            {
+                                MessageBox.Show("数据不一致");
+                            }
+                        }
+                        bmpallPtr += bmpalldata.Stride;
+                        bmpcPtr += bmpcdata.Stride;
+                    }
+                }
+                    MessageBox.Show(cnt + " bmpallptr 与bmpcPtr 一致");
+
+                bmp.UnlockBits(bmpalldata);
+                bmpc.UnlockBits(bmpcdata);
+
+                di.Add(new datainfo(namelist[cnt], startpos, bs.Length));
+                startpos += (int)bs.Length;
+                //if (buff.Length != bs.Length)
+                //{
+                //    MessageBox.Show("Error: buff.length=" + buff.Length + ",bs.length=" + bs.Length);
+                //}
+            }
+
+            bs.Flush();
+            fs.Flush();
+            bs.Close();
+            fs.Close();
+            return di;
+        } 
+        private static List<datainfo> ConstructImgData3(List<string> namelist, ref Rectangle imgrect)
+        {
+            List<datainfo> di = new List<datainfo>();
+            for (int cnt = 0; cnt < namelist.Count; cnt++)
+            {
+                Bitmap bmp = (Bitmap)Bitmap.FromFile(namelist[cnt]);
+                Bitmap bmpc = bmp.Clone(imgrect, bmp.PixelFormat);
+                BitmapData bmpalldata = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
+                BitmapData bmpcdata = bmpc.LockBits(new Rectangle(0, 0, imgrect.Width, imgrect.Height), ImageLockMode.ReadOnly, bmpc.PixelFormat);
+                unsafe  // Format1bppIndexed
+                {
+                    byte* bmpallPtr = (byte*)bmpalldata.Scan0.ToPointer();
+                    byte* bmpcPtr = (byte*)bmpcdata.Scan0.ToPointer();
+
+                    bmpallPtr += imgrect.Y * bmpalldata.Stride + imgrect.X / 8;
+                    for (int y = 0; y < bmpcdata.Height; y++)
+                    {
+                        for (int i = 0; i < bmpcdata.Width / 8; i++)
+                        {
+                            if (bmpallPtr[i] != bmpcPtr[i])
+                            {
+                                MessageBox.Show("数据不一致");
+                            }
+                        }
+                        bmpallPtr += bmpalldata.Stride;
+                        bmpcPtr += bmpcdata.Stride;
+                    }
+                }
+                bmp.UnlockBits(bmpalldata);
+                bmpc.UnlockBits(bmpcdata);
+            }
+            return di;
+        }
+        private static void ReadBitmap1(List<datainfo> di)
+        {
+            FileStream fs = new FileStream("img.data", FileMode.Open, FileAccess.Read);
+            BufferedStream bs = new BufferedStream(fs, 4096);
+            for (int i = di.Count - 1; i >= 0; i--)
+            {
+                byte[] buffer = new byte[di[i].count];
+                bs.Position = di[i].startpos;
+                bs.Read(buffer, 0, buffer.Length);
+
+                Stream sss = new MemoryStream();
+                sss.Write(buffer, 0, buffer.Length);
+                Bitmap imgc = (Bitmap)Bitmap.FromStream(sss);
+                imgc.Save(i + "_.tif");
+                sss.Dispose();
+                sss.Close();
+            }
+            bs.Close();
+            fs.Close();
+        }
+        private static void ReadBitmap2(List<datainfo> di)
+        {
+            FileStream fs = new FileStream("img.data", FileMode.Open, FileAccess.Read);
+            BufferedStream bs = new BufferedStream(fs, 4096);
+            for (int i = di.Count - 1; i >= 0; i--)
+            {
+                byte[] buffer = new byte[di[i].count];
+                bs.Position = di[i].startpos;
+                bs.Read(buffer, 0, buffer.Length);
+                unsafe{
+                    fixed( byte* p =  &buffer[0])
+                    {
+                        Bitmap Bmp = new Bitmap(392, 143, 52, PixelFormat.Format1bppIndexed, (IntPtr)p);
+                        Bmp.Save("load.tif");
+                    }
+                }
+                //Stream sss = new MemoryStream();
+                //sss.Write(buffer, 0, buffer.Length);
+                //Bitmap imgc = (Bitmap)Bitmap.FromStream(sss);
+                //imgc.Save(i + "_.tif");
+                //sss.Dispose();
+                //sss.Close();
+            }
+            bs.Close();
+            fs.Close();
+        }		
+
+        ///////////
+        private static List<datainfo> ConstructImgData(List<string> namelist, ref Rectangle imgrect)
+        {
+            //FileStream fs = new FileStream("img.data", FileMode.Append, FileAccess.Write);
+            MemoryStream ms = new MemoryStream();
+            BufferedStream bs = new BufferedStream(ms, 40960);
+
+            int startpos = 0;
+            List<datainfo> di = new List<datainfo>();
+            for (int cnt = 0; cnt < namelist.Count; cnt++)
+            {
+                Bitmap bmp = (Bitmap)Bitmap.FromFile(namelist[cnt]);
+                Bitmap imgc = bmp.Clone(imgrect, bmp.PixelFormat);
+                imgc.Save(bs, System.Drawing.Imaging.ImageFormat.Tiff);
+                bs.Flush();
+                byte[] buff = ms.ToArray();
+                //fs.Write(buff, 0, buff.Length);
+
+                //di.Add(new datainfo(namelist[cnt], startpos, bs.Length));
+                //startpos += (int)bs.Length;
+
+            }
+            ms.Flush();
+            bs.Flush();
+            ms.Close();
+            bs.Close();
+            //fs.Flush();
+            //fs.Close();
+            return di;
+        }
+        private static void ReadBitmap(List<datainfo> di)
+        {
+            FileStream fs = new FileStream("img.data", FileMode.Open, FileAccess.Read);
+            BufferedStream bs = new BufferedStream(fs, 4096);
+            for (int i = di.Count - 1; i >= 0; i--)
+            {
+                byte[] buffer = new byte[di[i].count];
+                bs.Position = di[i].startpos;
+                bs.Read(buffer, 0, buffer.Length);
+
+                Stream sss = new MemoryStream();
+                sss.Write(buffer, 0, buffer.Length);
+                Bitmap imgc = (Bitmap)Bitmap.FromStream(sss);
+                imgc.Save(i + "_.tif");
+                sss.Dispose();
+                sss.Close();
+            }
+            bs.Close();
+            fs.Close();
+        }		
+		private List<string> NameList(string filename){        	 
+        	List<string> namelist = new List<string>();
+        	if (File.Exists(filename)){
+	        	FileInfo fi = new FileInfo(filename);
+	            DirectoryInfo dirinfo = fi.Directory;
+	            string ext = fi.Extension;	            
+	            foreach (FileInfo f in dirinfo.GetFiles())
+	            	if (f.Extension.ToLower() == ".tif")
+	            		namelist.Add( f.FullName);    
+        	}
+        	return namelist;
+        }
+        public class datainfo
+        {
+            public datainfo(string kh, int startpos, long count)
+            {
+                this.kh = kh;
+                this.startpos = startpos;
+                this.count = count;
+            }
+            public string kh;
+            public int startpos;
+            public long count;
+
+            public override string ToString()
+            {
+                return kh + " " + startpos + " " + count;
+            }
+        }
+	}
+	
+}
